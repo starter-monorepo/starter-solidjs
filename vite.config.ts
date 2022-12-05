2bcbcc96b47252949041e8296d15f0339dfb3014
deuscx <1475453714@qq.com>
2022-12-05 22:21:47 +0800

    chore: init repo

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+
+import path from 'path'
+import { defineConfig } from 'vite'
+import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  plugins: [
+
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'solid-js',
+      ]
+    }),
+    // https://github.com/antfu/unocss
+    // see unocss.config.ts for config
+    Unocss(),
+  ],
+
+  // https://github.com/vitest-dev/vitest
+  test: {
+    environment: 'jsdom',
+  },
+})
